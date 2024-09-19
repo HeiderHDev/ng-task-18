68a72e8985110954e7f1236a5e75e455f2474515
HeiderHDev <heiderhrnndz@gmail.com>
2024-09-19 13:07:57 -0500

    feat: 🎸 Se configura firebase y tailwind en el proyecto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,25 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'ng-task-18-c304f',
+        appId: '1:743076476337:web:a88c5ba463d59ad5a07990',
+        storageBucket: 'ng-task-18-c304f.appspot.com',
+        apiKey: 'AIzaSyBfHNrZpj4t5DaXHLr9u6NDuzSLrv6vKP8',
+        authDomain: 'ng-task-18-c304f.firebaseapp.com',
+        messagingSenderId: '743076476337',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
