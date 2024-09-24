79f21b332bf1a3f9997ad8b532adc7716ab57c06
HeiderHDev <heiderhrnndz@gmail.com>
2024-09-24 15:39:07 -0500

    feat: 🎸 Se configura las rutas del proyecto, se agrega la ruta
    de auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'auth',
-  },
-  {
-    path: 'tasks',
+    loadChildren: () => import('./auth/features/auth.routes'),
   },
 ];
