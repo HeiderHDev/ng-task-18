a43cd1134c3a71f5c69a27108a7494945354c54f
HeiderHDev <heiderhrnndz@gmail.com>
2024-09-29 14:37:04 -0500

    feat: 🎸 Se crea servicio de autenticacion
    c

---
+++ src/app/auth/data-acces/auth.service.ts
@@ -0,0 +1,15 @@
+import { inject, Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
+import { UserSignUp } from '../interface/user-sign-up.interface';
+@Injectable()
+export class AuthService {
+  private _auth = inject(Auth);
+
+  signUp(user: UserSignUp) {
+    return createUserWithEmailAndPassword(
+      this._auth,
+      user.email,
+      user.password
+    );
+  }
+}
