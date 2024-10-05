38c8be440b3620b220ab5af7067acb656c198c01
HeiderHDev <heiderhrnndz@gmail.com>
2024-10-05 17:15:46 -0500

    feat: 🎸 Se creo metodo para Registrar nuevo usuario con correo

--- src/app/auth/data-acces/auth.service.ts
+++ src/app/auth/data-acces/auth.service.ts
@@ -1,15 +1,23 @@
 import { inject, Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
-import { UserSignUp } from '../interface/user-sign-up.interface';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from '@angular/fire/auth';
+import { User } from '../interface/user.interface';
 @Injectable()
 export class AuthService {
   private readonly _auth = inject(Auth);
 
-  signUp(user: UserSignUp) {
+  signUp(user: User) {
     return createUserWithEmailAndPassword(
       this._auth,
       user.email,
       user.password
     );
+  }
+
+  signIn(user: User) {
+    return signInWithEmailAndPassword(this._auth, user.email, user.password);
   }
 }
