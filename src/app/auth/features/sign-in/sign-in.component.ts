1c53c82c8773879d61fd4ead8acc3a2c018fa3c2
HeiderHDev <heiderhrnndz@gmail.com>
2024-10-05 17:17:41 -0500

    feat: 🎸 Se crea componente para crear cuenta o iniciar sesion

--- src/app/auth/features/sign-in/sign-in.component.ts
+++ src/app/auth/features/sign-in/sign-in.component.ts
@@ -1,17 +1,55 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { hasEmailError, isRequired } from '../../utils/validator';
+import { AuthService } from '../../data-acces/auth.service';
+import { toast } from 'ngx-sonner';
+import { Router, RouterLink } from '@angular/router';
+import { FormSignIn } from '../../interface/form-sign-in.interface';
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
+  providers: [AuthService],
   templateUrl: './sign-in.component.html',
   styles: ``,
 })
 export default class SignInComponent {
-  private _formBuilder = inject(FormBuilder);
+  private readonly _formBuilder = inject(FormBuilder);
+  private readonly _authService = inject(AuthService);
+  private readonly _router = inject(Router);
 
-  form = this._formBuilder.group<any>({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required]],
+  isRequired(field: 'email' | 'password') {
+    return isRequired(field, this.form);
+  }
+
+  hasEmailError() {
+    return hasEmailError(this.form);
+  }
+
+  form = this._formBuilder.group<FormSignIn>({
+    email: this._formBuilder.control('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: this._formBuilder.control('', [Validators.required]),
   });
+
+  hasRequiredError(field: string) {
+    const control = this.form.get(field);
+    return control?.hasError('required') && control.touched;
+  }
+
+  async onSubmit() {
+    if (this.form.invalid) return;
+    try {
+      const { email, password } = this.form.value;
+      if (!email || !password) return;
+      await this._authService.signIn({ email, password });
+      toast.success('Hola nuevamente');
+      this._router.navigateByUrl('/tasks');
+    } catch (error) {
+      console.error(error);
+      toast.success('Error al crear la cuenta');
+    }
+  }
 }
--- src/app/auth/features/sign-up/sign-up.component.ts
+++ src/app/auth/features/sign-up/sign-up.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../data-acces/auth.service';
+import { Router, RouterLink } from '@angular/router';
+import { hasEmailError, isRequired } from '../../utils/validator';
+import { FormSignUp } from '../../interface/form-sign-up.interface';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
+  providers: [AuthService],
   templateUrl: './sign-up.component.html',
   styles: ``,
 })
-export default class SignUpComponent {}
+export default class SignUpComponent {
+  private readonly _formBuilder = inject(FormBuilder);
+  private readonly _authService = inject(AuthService);
+  private readonly _router = inject(Router);
+
+  isRequired(field: 'email' | 'password') {
+    return isRequired(field, this.form);
+  }
+
+  hasEmailError() {
+    return hasEmailError(this.form);
+  }
+
+  form = this._formBuilder.group<FormSignUp>({
+    email: this._formBuilder.control('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: this._formBuilder.control('', [Validators.required]),
+  });
+
+  hasRequiredError(field: string) {
+    const control = this.form.get(field);
+    return control?.hasError('required') && control.touched;
+  }
+
+  async onSubmit() {
+    if (this.form.invalid) return;
+    try {
+      const { email, password } = this.form.value;
+      if (!email || !password) return;
+      await this._authService.signUp({ email, password });
+      toast.success('Cuenta creada exitosamente');
+      this._router.navigateByUrl('/tasks');
+    } catch (error) {
+      console.error(error);
+      toast.success('Error al crear la cuenta');
+    }
+  }
+}
