f0fc3bf19961523849d7154d9c363ff68fdf06dc
HeiderHDev <heiderhrnndz@gmail.com>
2024-10-06 10:55:30 -0500

    feat: 🎸 Se crea metodo para iniciar sesion con cuenta de google

--- src/app/auth/data-acces/auth.service.ts
+++ src/app/auth/data-acces/auth.service.ts
@@ -2,7 +2,9 @@ import { inject, Injectable } from '@angular/core';
 import {
   Auth,
   createUserWithEmailAndPassword,
+  GoogleAuthProvider,
   signInWithEmailAndPassword,
+  signInWithPopup,
 } from '@angular/fire/auth';
 import { User } from '../interface/user.interface';
 @Injectable()
@@ -19,5 +21,13 @@ export class AuthService {
 
   signIn(user: User) {
     return signInWithEmailAndPassword(this._auth, user.email, user.password);
+  }
+
+  signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    //Tip: para que la cuenta de google sea seleccionada, si tienes sola una cuenta, debes agregar el parametro prompt: 'select_account'
+    // para poder crear uns cuenta nueva o iniciar sesión con una cuenta existente
+    // provider.setCustomParameters({ prompt: 'select_account' });
+    return signInWithPopup(this._auth, provider);
   }
 }
